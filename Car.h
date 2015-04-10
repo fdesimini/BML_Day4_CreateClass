adf4dc958cdb666dd2310fad7823faf7be4e03ae
fdesimini <fdesimini@users.noreply.github.com>
2015-04-10 13:39:31 -0400

    created the Car class ( Car : NSObject)
    declared a property called 'model'
    
    declared a method call 'drive'

---
+++ Car.h
@@ -0,0 +1,24 @@
+//
+//  Car.h
+//  CarClass
+//
+//  Created by Frank Desimini on 2015-04-10.
+//  Copyright (c) 2015 Frank Desimini. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Car : NSObject {
+    
+// Protected instance variables (not recommended)
+    
+}
+
+@property (copy) NSString *model;
+
+- (void) drive;
+
+
+
+
+@end
---
+++ Car.m
@@ -0,0 +1,13 @@
+//
+//  Car.m
+//  CarClass
+//
+//  Created by Frank Desimini on 2015-04-10.
+//  Copyright (c) 2015 Frank Desimini. All rights reserved.
+//
+
+#import "Car.h"
+
+@implementation Car
+
+@end
