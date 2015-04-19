d3ba4691ef0edc432b810e85d43851abee5dee22
fdesimini <fdesimini@users.noreply.github.com>
2015-04-19 14:06:44 -0400

    adding a new class called Motorcycle

---
+++ CarClass/Motorcycle.h
@@ -0,0 +1,18 @@
+//
+//  Motorcycle.h
+//  CarClass
+//
+//  Created by Frank Desimini on 2015-04-19.
+//  Copyright (c) 2015 Frank Desimini. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Motorcycle : NSObject
+
+@property (copy) NSString *model;
+
+- (void) drive;
+
+
+@end
---
+++ CarClass/Motorcycle.m
@@ -0,0 +1,13 @@
+//
+//  Motorcycle.m
+//  CarClass
+//
+//  Created by Frank Desimini on 2015-04-19.
+//  Copyright (c) 2015 Frank Desimini. All rights reserved.
+//
+
+#import "Motorcycle.h"
+
+@implementation Motorcycle
+
+@end
